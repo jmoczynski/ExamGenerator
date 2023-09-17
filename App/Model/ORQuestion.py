8f5ac6d0b78e8b49605baa05aad9b769f285270c
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 15:13:07 -0400

    added ORQuestion class and ORQuestionTests suite

---
+++ App/Model/ORQuestion.py
@@ -0,0 +1,24 @@
+class ORQuestion:
+
+    def __init__(self, number=None, question=None, suggested_solution=None):
+        self._number = number
+        self._question = question
+        self._suggested_solution = suggested_solution
+
+    def get_number(self):
+        pass
+
+    def get_question(self):
+        pass
+
+    def get_suggested_solution(self):
+        pass
+
+    def set_number(self, number: int):
+        pass
+
+    def set_question(self, question: str):
+        pass
+
+    def set_suggested_solution(self, suggested_solution: str):
+        pass
---
+++ App/Model/ORQuestionTests.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_constructors_and_accessors(self):
+        self.assertTrue(False)
+
+    def test_mutators(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
