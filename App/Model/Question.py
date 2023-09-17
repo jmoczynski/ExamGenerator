a90941f5a29d60a75702b6100482a48ff2236c3b
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 08:18:30 -0400

    worked on unit tests for Question class

--- App/Model/Question.py
+++ App/Model/Question.py
@@ -1,19 +1,43 @@
+# Question class
 class Question:
 
+    # constructor for Question
     def __init__(self, number=None, question=None, answers=None, solution=None):
         self._number = number
         self._question = question
         self._answers = answers
         self._solution = solution
 
+    # accessor for number property of Question
     def get_number(self):
         return None
 
+    # accessor for question property of Question
     def get_question(self):
         return None
 
+    # accessor for answers property of Question
     def get_answers(self):
         return None
 
+    # accessor for solution property of Question
     def get_solution(self):
         return None
+
+    # mutator for number property of Question
+    def set_number(self, number: int):
+        pass
+
+    # mutator for question property of Question
+    def set_question(self, question: str):
+        pass
+
+    # mutator for answers property of Question
+    def set_answers(self, answers: list):
+        pass
+
+
+    # mutator for solution property of Question
+    def set_solution(self, solution: int):
+        pass
+
--- App/Model/QuestionTests.py
+++ App/Model/QuestionTests.py
@@ -18,5 +18,43 @@ class QuestionTests(unittest.TestCase):
         self.assertEqual(testQ2.get_answers(), testAList2)
         self.assertEqual(testQ2.get_solution(), testAList2[0])
 
+    def test_mutators(self):
+        testQ1 = Question()
+        testNumber = 1
+        testQ1.set_number(testNumber)
+        self.assertEqual(testQ1.get_number(), testNumber)
+        testQuestion1 = "What is the question?"
+        testQ1.set_question(testQuestion1)
+        self.assertEqual(testQ1.get_number(), 1)
+        self.assertEqual(testQ1.get_question(), testQuestion1)
+        testAList1_1 = []
+        testAList1_2 = ["A", "B", "C", "D"]
+        self.assertRaises(ValueError, testQ1.set_answers(testAList1_1))
+        testQ1.set_answers(testAList1_2)
+        self.assertEqual(testQ1.get_number(), 1)
+        self.assertEqual(testQ1.get_question(), testQuestion1)
+        self.assertEquals(testQ1.get_answers, testAList1_2)
+        testSolution1_1= -10
+        testSolution1_2 = -1
+        testSolution1_3 = 0
+        testSolution1_4 = 1
+        testSolution1_5 = 3
+        testSolution1_6 = 4
+        testSolution1_7 = 10
+        self.assertRaises(ValueError, testSolution1_1)
+        self.assertRaises(ValueError, testSolution1_2)
+        self.assertRaises(ValueError, testSolution1_6)
+        self.assertRaises(ValueError, testSolution1_7)
+        testQ1.set_solution(testSolution1_3)
+        self.assertEqual(testQ1.get_number(), 1)
+        self.assertEqual(testQ1.get_question(), testQuestion1)
+        self.assertEquals(testQ1.get_answers(), testAList1_2)
+        self.assertEquals(testQ1.get_solution(), testSolution1_3)
+        testQ1.set_solution(testSolution1_4)
+        self.assertEquals(testQ1.get_solution(), testSolution1_4)
+        testQ1.set_solution(testSolution1_5)
+        self.assertEquals(testQ1.get_solution(), testSolution1_5)
+
+
 if __name__ == '__main__':
     unittest.main()
