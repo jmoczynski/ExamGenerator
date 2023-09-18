d4c493c74d860e63c734abe9fd530b351166365a
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 20:14:50 -0400

    reorganizing structure, implemented Question class

--- App/Model/Question.py
+++ App/Model/Question.py
@@ -1,10 +1,12 @@
 class Question:
 
     def __init__(self, question=None):
-        self.question = question
+        self._question = question
 
     def get_question(self):
-        pass
+        return self._question
 
     def set_question(self, question: str):
-        pass
+        if len(question) < 1:
+            raise ValueError("Question property of Question cannot be empty.")
+        self._question = question
