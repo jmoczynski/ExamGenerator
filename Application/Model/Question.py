8174d90f3f6dfbe524ce35930f76700862c5cfd4
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-18 19:30:33 -0400

    added Question comparison methods

--- Application/Model/Question.py
+++ Application/Model/Question.py
@@ -10,3 +10,9 @@ class Question:
         if len(question) < 1:
             raise ValueError("Question property of Question cannot be empty.")
         self._question = question
+
+def compare_questions(self, other: Question):
+    pass
+
+def same_question(self, other: Question):
+    pass
