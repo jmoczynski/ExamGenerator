721a5accc68978ddb6fc8c7e274170eaf7959a08
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-18 19:08:13 -0400

    added rest of initial frames

--- Application/View/App.py
+++ Application/View/App.py
@@ -1,15 +1,30 @@
 import tkinter as tk
 from tkinter import font
 from Application.Controller.AppController import AppController
+from Application.View.AboutFrame import AboutFrame
+from Application.View.DeleteQuestionFrame import DeleteQuestionFrame
+from Application.View.ModifyQuestionFrame import ModifyQuestionFrame
+from Application.View.EditQuestionListFrame import EditQuestionListFrame
+from Application.View.ExportExamFrame import ExportExamFrame
+from Application.View.HelpFrame import HelpFrame
+from Application.View.ImportCSVFrame import ImportCSVFrame
+from Application.View.NewQuestionFrame import NewQuestionFrame
 from Application.View.OpenExamFrame import OpenExamFrame
 from Application.View.NewExamFrame import NewExamFrame
+from Application.View.PreviewExamFrame import PreviewExamFrame
+from Application.View.RandomizeQuestionsFrame import RandomizeQuestionsFrame
+from Application.View.SaveExamAsFrame import SaveExamAsFrame
+from Application.View.SaveExamFrame import SaveExamFrame
 from Application.View.StartFrame import StartFrame
 
 class App:
 
     def __init__(self, *args, **kwargs):
         self._window = tk.Tk()
-        self._frame_list = [StartFrame, NewExamFrame, OpenExamFrame]
+        self._frame_list = [StartFrame, NewExamFrame, OpenExamFrame, SaveExamFrame, SaveExamAsFrame,
+                            PreviewExamFrame, ExportExamFrame, NewQuestionFrame, ModifyQuestionFrame,
+                            DeleteQuestionFrame, EditQuestionListFrame, ImportCSVFrame, RandomizeQuestionsFrame,
+                            HelpFrame, AboutFrame]
         self.controller = AppController(frames=self._frame_list)
 
         self._menubar = tk.Menu(self._window)
@@ -30,25 +45,25 @@ class App:
         file.add_command(label="Back to Start", command=lambda: self.controller.show_frame("StartFrame"))
         file.add_command(label="New Exam", command=lambda: self.controller.show_frame("NewExamFrame"))
         file.add_command(label="Open Exam", command=lambda: self.controller.show_frame("OpenExamFrame"))
-        file.add_command(label="Save Exam", command=None)
-        file.add_command(label="Save Exam As", command=None)
-        file.add_command(label="Preview Exam", command=None)
-        file.add_command(label="Export Exam", command=None)
+        file.add_command(label="Save Exam", command=lambda: self.controller.show_frame("SaveExamFrame"))
+        file.add_command(label="Save Exam As", command=lambda: self.controller.show_frame("SaveExamAsFrame"))
+        file.add_command(label="Preview Exam", command=lambda: self.controller.show_frame("PreviewExamFrame"))
+        file.add_command(label="Export Exam", command=lambda: self.controller.show_frame("ExportExamFrame"))
         file.add_command(label="Exit", command=lambda: exit(0))
 
         edit = tk.Menu(self._menubar, tearoff=0)
         self._menubar.add_cascade(label="Edit", menu=edit)
-        edit.add_command(label="New Question", command=None)
-        edit.add_command(label="Modify Question", command=None)
-        edit.add_command(label="Delete Question", command=None)
-        edit.add_command(label="Edit Question List", command=None)
-        edit.add_command(label="Import CSV", command=None)
-        edit.add_command(label="Randomize Questions", command=None)
+        edit.add_command(label="New Question", command=lambda: self.controller.show_frame("NewQuestionFrame"))
+        edit.add_command(label="Modify Question", command=lambda: self.controller.show_frame("ModifyQuestionFrame"))
+        edit.add_command(label="Delete Question", command=lambda: self.controller.show_frame("DeleteQuestionFrame"))
+        edit.add_command(label="Edit Question List", command=lambda: self.controller.show_frame("EditQuestionListFrame"))
+        edit.add_command(label="Import CSV", command=lambda: self.controller.show_frame("ImportCSVFrame"))
+        edit.add_command(label="Randomize Questions", command=lambda: self.controller.show_frame("RandomizeQuestionsFrame"))
 
         help = tk.Menu(self._menubar, tearoff=0)
         self._menubar.add_cascade(label="Help", menu=help)
-        help.add_command(label="Help", command=None)
-        help.add_command(label="About", command=None)
+        help.add_command(label="Help", command=lambda: self.controller.show_frame("HelpFrame"))
+        help.add_command(label="About", command=lambda: self.controller.show_frame("AboutFrame"))
 
     def view(self):
         self._window.mainloop()
---
+++ Application/View/ExportExamFrame.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import Frame
+
+class ExportExamFrame(Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent,  background="White")
+        self.controller = controller
+        self.title_font = tk.font.Font(family="Helvetica", size="32", weight="bold")
+        title = tk.Label(self, text="Export Exam", font=self.title_font, anchor="n")
+        title.pack(side="top", fill="x")
