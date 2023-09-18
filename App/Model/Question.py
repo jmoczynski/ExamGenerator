e2f5931f9f66e342e075841086f22280151a4b31
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 20:13:28 -0400

    reorganizing structure, add Question tests

---
+++ App/Model/Question.py
@@ -0,0 +1,10 @@
+class Question:
+
+    def __init__(self, question=None):
+        self.question = question
+
+    def get_question(self):
+        pass
+
+    def set_question(self, question: str):
+        pass
