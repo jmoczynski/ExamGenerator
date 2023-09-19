fdc01a835093a6d0743f0a95b4d38013d59bba50
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-18 21:17:05 -0400

    implemented QuestionComparator class

--- Application/Model/QuestionComparator.py
+++ Application/Model/QuestionComparator.py
@@ -8,44 +8,44 @@ from Application.Model.Question import Question
 class QuestionComparator:
 
     def __init__(self, q1: Question, q2: Question):
-        self.q1 = q1
-        self.q2 = q2
+        self._q1 = q1
+        self._q2 = q2
 
     def compare_questions(self):
-        if self.q1.__class__ != self.q2.__class__:
+        if self._q1.__class__ != self._q2.__class__:
             return -4
-        if self.q1.__class__ == MCQuestion:
-            if self.q1.get_question() != self.q2.get_question():
+        if self._q1.__class__ == MCQuestion:
+            if self._q1.get_question() != self._q2.get_question():
                 return -3
-            if collections.Counter(self.q1.get_answers()) != collections.Counter(self.q2.get_answers()):
+            if collections.Counter(self._q1.get_answers()) != collections.Counter(self._q2.get_answers()):
                 return -2
-            if collections.Counter(self.q1.get_solutions()) != collections.Counter(self.q2.get_solutions()):
+            if collections.Counter(self._q1.get_solutions()) != collections.Counter(self._q2.get_solutions()):
                 return -1
             return 0
 
-        if self.q1.__class__ == ORQuestion:
-            if self.q1.get_question() != self.q2.get_question():
+        if self._q1.__class__ == ORQuestion:
+            if self._q1.get_question() != self._q2.get_question():
                 return -2
-            if self.q1.get_suggested_solution() != self.q2.get_suggested_solution():
+            if self._q1.get_suggested_solution() != self._q2.get_suggested_solution():
                 return -1
             return 0
 
 
     def same_question(self):
-        if self.q1.__class__ != self.q2.__class__:
+        if self._q1.__class__ != self._q2.__class__:
             return False
-        if self.q1.__class__ == MCQuestion:
-            if self.q1.get_question() != self.q2.get_question():
+        if self._q1.__class__ == MCQuestion:
+            if self._q1.get_question() != self._q2.get_question():
                 return False
-            if collections.Counter(self.q1.get_answers()) != collections.Counter(self.q2.get_answers()):
+            if collections.Counter(self._q1.get_answers()) != collections.Counter(self._q2.get_answers()):
                 return False
-            if collections.Counter(self.q1.get_solutions()) != collections.Counter(self.q2.get_solutions()):
+            if collections.Counter(self._q1.get_solutions()) != collections.Counter(self._q2.get_solutions()):
                 return False
             return True
 
-        if self.q1.__class__ == ORQuestion:
-            if self.q1.get_question() != self.q2.get_question():
+        if self._q1.__class__ == ORQuestion:
+            if self._q1.get_question() != self._q2.get_question():
                 return False
-            if self.q1.get_suggested_solution() != self.q2.get_suggested_solution():
+            if self._q1.get_suggested_solution() != self._q2.get_suggested_solution():
                 return False
             return True
