90f39b3d1aca3af687247b0cae600ce62faccf70
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-19 16:49:04 -0400

    worked on NewQuestionFrame

--- Application/View/NewQuestionFrame.py
+++ Application/View/NewQuestionFrame.py
@@ -47,14 +47,30 @@ class NewQuestionFrame(Frame):
         self.question_check = tk.Label(question_frame, fg="Red")
         self.question_check.config(text="Please check your question using the button below.", fg="Red")
         self.question_check.pack(side="top", anchor="nw")
+        self.question_button = tk.Button(question_frame, text="Update Question")
+        self.question_button.config(command=lambda: self.update_question())
+        self.question_button.pack(side="top", anchor="nw")
+        self.question = ""
 
+        self.num_answers_button = None
+
+        self.answers_button = None
+        self.answers_label = None
         self.answer_frame = None
         self.answer_message = None
+        self.answers = []
 
         self.mc_solutions_frame = None
         self.or_solution_frame = None
 
         self.solutions = dict()
+        self.solutions_button = None
+
+    def update_question(self):
+        if self.check_question():
+            self.question = self.question_text.get("1.0", "end-1c")
+            print(self.question)
+
 
     def _on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
@@ -70,6 +86,8 @@ class NewQuestionFrame(Frame):
             self.previous_selection = self.controller.selection(selected)
         result = self.check_question()
         print(result)
+        if not result and self.num_answers_button is not None:
+            self.num_answers_button.config(state="disabled")
         if self.controller.get_question_type_selection() == 1:
             if self.answer_frame is None:
                 self.answer_frame = tk.LabelFrame(self.canvas, text="3. Multiple Choice Answers", background="White")
@@ -90,7 +108,7 @@ class NewQuestionFrame(Frame):
             self.answer_message = tk.Label(self.answer_frame, fg="Red", background="White")
             self.answer_message.config(text="Amount of Choices cannot be empty or 0.")
             self.answer_message.pack(side="top", anchor="nw")
-            self.num_answers_button = tk.Button(self.answer_frame, text="Enter Choice Amount", command=lambda: self.set_choices_entry(num_answers.get()))
+            self.num_answers_button = tk.Button(self.answer_frame, text="Enter Choice Amount", state="disabled", command=lambda: self.set_choices_entry(num_answers.get()))
             self.num_answers_button.pack(side="top", anchor="nw")
         elif self.or_solution_frame is not None:
             tk.Label(self.or_solution_frame, text="Enter suggested solution.").pack(side="top", anchor="nw")
@@ -148,11 +166,19 @@ class NewQuestionFrame(Frame):
         for i in range(len(self.answers)):
             self.solutions[str(i)] = tk.IntVar()
             solution_choice_box = tk.Checkbutton(self.mc_solutions_frame, variable=self.solutions[str(i)], text=self.answers[i].get("1.0", "end-1c"), onvalue=1, offvalue=0)
-            solution_choice_box.config(command=lambda: self.update_solution(i, solution_choice_box))
+            solution_choice_box.config(command=lambda: self.print_solutions())
             solution_choice_box.pack(side="top", anchor="nw")
+        self.solutions_button = tk.Button(self.mc_solutions_frame, text="Submit Solutions", command=lambda: self.create_mc_question())
+        self.solutions_button.pack(side="top", anchor="nw")
 
-    def update_solution(self, i, box):
-        pass
+    def print_solutions(self):
+        for j in range(len(self.solutions)):
+            print(str(j+1) + ": " + str(self.solutions[str(j)].get()))
+
+    def create_mc_question(self):
+        self.solutions_button.config(state="disabled")
+        self.controller.create_mc_question(self.question, self.answers, self.solutions)
+
 
     def check_question(self):
         result = False
@@ -160,8 +186,12 @@ class NewQuestionFrame(Frame):
         if len(question) < 1 or len(question.strip()) < 1:
             self.question_check.config(text="Question must not be empty.", fg="Red")
             self.question_check.pack(side="top", anchor="nw")
+            if self.num_answers_button is not None:
+                self.num_answers_button.config(state="disabled")
         else:
             self.question_check.config(text="Question is valid.", fg="Green")
             self.question_check.pack(side="top", anchor="nw")
+            if self.num_answers_button is not None:
+                self.num_answers_button.config(state="active")
             result = True
         return result
